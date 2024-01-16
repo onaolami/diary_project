d354d971c15c4243b9436d883eed6a7dc346594b
onaolami <onaola.omolori@gmail.com>
2024-01-16 13:01:10 +0100

    updated successfully

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const auth = require("./routes/auth");
 const diary = require("./routes/diary")
+const verifyAuth = require("./middleware/verifyAuth")
 
 
 app.use(express.json());
 app.use("/auth",auth);
-app.use("/diary", diary);
+app.use("/diary",verifyAuth, diary);
+
 
 
 app.listen(5001,()=>{
---
+++ middleware/verifyAuth.js
@@ -0,0 +1,24 @@
+var jwt = require("jsonwebtoken")
+
+const verifyAuth = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+
+  if (authHeader) {
+    const token = authHeader.replace("Bearer ", "");
+
+    const valid = jwt.verify(token, "mySecretKey");
+
+    if (valid) {
+      next();
+    }else{
+       return res.send("Unauthorized request") 
+    }
+      
+
+  }else{
+    return res.status(401).send("Unauthorized request")
+  }
+};
+
+
+module.exports = verifyAuth;
